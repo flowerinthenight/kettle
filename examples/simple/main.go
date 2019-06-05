2dca9c62f526ee6fc037a8718878e3bdc26db8be
flowerinthenight <chew.esmero@mobingi.com>
2019-06-05 18:58:23 +0900

    wip sample

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+
+	"github.com/flowerinthenight/kettle"
+)
+
+func doMaster(v interface{}) error {
+	c := v.(string)
+	log.Println("doing master work with context:", c)
+	return nil
+}
 
 func main() {
-	log.Println("hello")
+	s, err := kettle.New(kettle.WithVerbose(true))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	in := kettle.StartInput{
+		Master:    doMaster,
+		MasterCtx: "masterctx",
+		Quit:      make(chan error),
+		Done:      make(chan error),
+	}
+
+	err = s.Start(&in)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(time.Second * 40)
+	in.Quit <- nil // terminate
+	<-in.Done      // wait
 }
