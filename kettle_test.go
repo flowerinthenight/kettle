07874025098bc6583a00d03cdce8e9b1e03cc1f0
flowerinthenight <chew.esmero@mobingi.com>
2019-06-06 11:52:40 +0900

    update test

--- kettle_test.go
+++ kettle_test.go
@@ -2,33 +2,39 @@ package kettle
 
 import (
 	"log"
+	"os"
 	"testing"
 	"time"
 )
 
 func TestGen(t *testing.T) {
-	s, err := New(withVerbose(true))
+	if host := os.Getenv("REDIS_HOST"); host == "" {
+		log.Println("ho redis host:port")
+		return
+	}
+
+	k, err := New(withVerbose(true))
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	in := StartInput{
 		Master: func(v interface{}) error {
-			c := v.(string)
-			log.Println(c)
+			kt := v.(*Kettle)
+			log.Println("from master, name:", kt.Name())
 			return nil
 		},
-		MasterCtx: "masterctx",
+		MasterCtx: k,
 		Quit:      make(chan error),
 		Done:      make(chan error),
 	}
 
-	err = s.Start(&in)
+	err = k.Start(&in)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	time.Sleep(time.Second * 40)
+	time.Sleep(time.Second * 5)
 	in.Quit <- nil // terminate
 	<-in.Done      // wait
 }
